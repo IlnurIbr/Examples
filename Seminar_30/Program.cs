e911183c6e386e1ec409a1ebbe6c9772e868f530
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-25 02:32:26 +0500

    Массив из 0 и 1

---
+++ Seminar_30/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.*/
+Console.Clear();
+
+int[] array = NewArray();
+Console.Write($"[{string.Join(",", array)}]");
+
+int[] NewArray()
+{
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{ 
+array[i] = new Random().Next(2);
+}
+return array;
+}
