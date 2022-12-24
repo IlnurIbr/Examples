e29793cd4fe501c7296242d0304fac37b52c89c0
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-24 23:08:46 +0500

    Количество цифр в числе

---
+++ Seminar_26/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число
+и выдает количество цифр в числе.*/
+int QuantityNumbers(int a)
+{
+    int s = 0;
+    for (int count = 1; a >= 1; count++)
+        {
+            a = a/10;
+            s = count;
+        }
+        return s;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+
+int a = EnterData("Введите число");
+int q = QuantityNumbers(a);
+Console.WriteLine(q);
