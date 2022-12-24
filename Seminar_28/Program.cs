65501d84ff64a127d939ddf34b8eba80baca121b
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-25 02:31:47 +0500

    Произведение чисел от 1 до N

---
+++ Seminar_28/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число N
+ и выдает произведение чисел от 1 до N*/
+Console.Clear();
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+
+int ProductOfNumbers(int n)
+{
+int product = 1;
+
+    for (int count = 1; count <= n; count++)
+    {
+        product = product*count;
+    }
+    return product;
+}
+
+ int n = EnterData("Введите число: ");
+ int c = ProductOfNumbers(n);
+ Console.WriteLine($"Произведение чисел от 1 до {n} = {c}");
