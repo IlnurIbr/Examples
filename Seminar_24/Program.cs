76e45da7b92fa225bd7de7f8e21e74e2fcd0aed0
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-24 22:42:32 +0500

    Сумма чисел от 1 до N

---
+++ Seminar_24/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 24: Написать программу, которая принимает число А и выдает сумму от 1 до А
+7 - 28
+4 - 10
+8 - 36*/
+
+int SumOfNumbers(int a)
+{
+    int sum = 0;
+
+    for (int count = 0; count <= a; count++)
+    {
+        sum = sum + count;
+    }        
+    return sum;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int a = int.Parse(Console.ReadLine());
+    return(a);    
+}
+
+int a = EnterData("Введите число");
+int s = SumOfNumbers(a);
+Console.WriteLine($"Сумма чисел = от 1 до {a} = {s}");
