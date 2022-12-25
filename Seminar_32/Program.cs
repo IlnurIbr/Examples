e0f36302552e404c1a06ed944d32cb46c33ec2fe
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-25 16:31:11 +0500

    замена элементов массива на противоположенные

---
+++ Seminar_32/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: положительные элементы
+замените на соответсвующие отрицательные элемент*/
+
+int[] NegaiveNewArray(int[] array)
+{
+     int[] array2 = new int[array.Length];
+    for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = -array[i];
+    }
+    return array2;
+}
+
+int[] NewArray(int amount, int begin, int end)
+{
+int[] array = new int[amount];
+    for (int i = 0; i < amount; i++)
+    {
+        array[i] = new Random().Next(begin, end);
+    }
+    return array;
+}
+
+
+int[] array = NewArray(4, -10, 10);
+Console.WriteLine($"[{string.Join(",", array)}]");
+int[] array2 = NegaiveNewArray(array);
+Console.WriteLine($"[{string.Join(",", array2)}]");
