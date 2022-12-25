39539e46ad7774bd5b36ef86f7b307ac3aa4d378
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-26 01:58:44 +0500

    Количество элементов массива в интервале от до

---
+++ Seminar_35/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежать 
+в отрезке [10, 99]*/
+
+int CountInterval(int[] array, int begin, int end)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (array[i] > begin && array[i] < end)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+int[] NewArray(int amount, int begin, int end)
+{
+    int[] array = new int[amount];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(begin, end);
+    }
+    return array; 
+}
+
+int[] array = NewArray(123, -500, 500);
+
+Console.WriteLine($"[{string.Join(",", array)}]");
+
+int count = CountInterval(array, 10, 99);
+
+Console.WriteLine($"Количество элементов массива в интервале от 10 до 99: {count} ");
