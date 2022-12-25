785f23ab2f67c86712139c50037ea060860b2c5f
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-25 17:37:30 +0500

    Проверка есть ли число в массиве

---
+++ Seminar_33/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 33: Задайте массив. Напишите программу, которая определяет, 
+присутствует ли заданное число в массиве.*/
+
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string MassiveNumber(int[] array, int a)
+{
+    string answer = "Нет";
+    for (int i = 0; i < array.Length; i++)
+    
+    {
+         if (array[i] == a)
+         {
+            answer = "Да";
+         }
+    }
+    return answer;
+    }
+
+int[] array = {6, 7, 19, 435, 3};
+
+int a = EnterData("Введите число: ");
+
+string answer = MassiveNumber(array, a);
+
+Console.WriteLine(answer);
