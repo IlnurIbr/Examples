4a834801f2d09cdae401b5734873d22b838308dc
Ilnur Ibragimov <ilnur7081991@mail.ru>
2022-12-29 23:25:45 +0500

    Задать двумерный элементы n+m

---
+++ Seminar_48/Program.cs
@@ -0,0 +1,44 @@
+﻿
+void PrintArray(int[,] arr)
+{
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");;
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+
+//Запрос количества строк
+int rows = EnterData("Введите количество строк: ");
+//Запрос количества столбцов
+int columns = EnterData("Введите количество столбцов: ");
+//Создаем двумерный массив и заполняем его случайными числами
+int[,] matrix = FillArray(rows, columns, -100, 100);
+//ВВодим на экран
+PrintArray(matrix);
