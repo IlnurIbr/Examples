238700901e99dcf7c46f088aa8aacb4895023f68
Ilnur Ibragimov <ilnur7081991@mail.ru>
2023-01-02 22:05:35 +0500

    Частотный словарь

---
+++ Seminar_57/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 57: Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, сколько раз встречается элемент
+входных данных*/
+void NumberCount(int[,] arr, int number)
+{
+    int count = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i,j] == number)
+            {
+             count++;
+            }         
+
+        }
+              
+    }
+    if (count > 0) Console.WriteLine($"{number} встречается {count} раз");
+}
+
+void PrintArray(int[,] arr)
+{
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(begin, end+1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+
+//Запрос количества строк
+int rows = EnterData("Введите количество строк: ");
+//Запрос количества столбцов
+int columns = EnterData("Введите количество столбцов: ");
+//Создаем двумерный массив и заполняем его случайными числами
+int[,] matrix = FillArray(rows, columns, 0, 9);
+//ВВодим на экран
+PrintArray(matrix);
+Console.WriteLine();
+for (int i = 0; i < 10; i++) 
+{
+NumberCount(matrix, i);
+}
+
